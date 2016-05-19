6e9a78d5ebd97a3f866c407623b48aff8e8a0bf9
Takahiro Takashima <takashima0411@gmail.com>
2016-05-19 12:30:08 +0000

    today i learned

--- javascript/react/championlist/src/app.jsx
+++ javascript/react/championlist/src/app.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { List } from './list.jsx'
 
 var App = React.createClass({
 	render: function(){
 		return (
 				<div>
 					<h1>Champion list</h1>
+					<List />
 				</div>
 			);
 	}
---
+++ javascript/react/championlist/src/list.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export var List = React.createClass({
+	render: function(){
+		return (
+				<div>
+					<ul>
+						<li>hoge</li>
+						<li>hoge</li>
+						<li>hoge</li>
+						<li>hoge</li>
+					</ul>
+				</div>
+			);
+	}
+});
