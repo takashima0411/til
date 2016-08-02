fccdb20a1e5e13a42ce777e45c64897d323d07d1
Takahiro Takashima <takashima0411@gmail.com>
2016-08-02 23:29:13 +0900

    today i learned

---
+++ wpf/ExpenseIt/ExpenseIt/ExpenseItHome.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace ExpenseIt
+{
+    /// <summary>
+    /// ExpenseItHome.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ExpenseItHome : Page
+    {
+        public ExpenseItHome()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // View Expense Report
+            if (this.peopleListBox.SelectedItem != null)
+            {
+                ExpenseReportPage expenseReportPage = new ExpenseReportPage(this.peopleListBox.SelectedItem);
+                this.NavigationService.Navigate(expenseReportPage);
+            }
+            else
+            {
+                Debug.WriteLine("要素が選択されていません。");
+            }
+        }
+    }
+
+}
