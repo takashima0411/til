24be732feb640442810a38dd9ca4a33ef3307dc0
takashima0411 <takashima0411@gmail.com>
2021-07-16 15:59:59 +0900

    refactoring: remove redundant variable

--- java/algorithm/LinearSearch/src/main/java/LinearSearch.java
+++ java/algorithm/LinearSearch/src/main/java/LinearSearch.java
@@ -11,14 +11,12 @@ class LinearSearch {
      * @return 発見出来た場合、その要素が存在する添字。見つからなければ-1。
      */
     static int linearSearch(final int[] array, final int n, final int x) {
-        int answer = NOT_FOUND;
-
         for (int i = 0; i < n; i++) {
             if (array[i] == x) {
                 return i;
             }
         }
 
-        return answer;
+        return NOT_FOUND;
     }
 }
